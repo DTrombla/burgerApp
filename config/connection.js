fbc7ed09692e317e8c640dae85a0d7a9ed367022
DTrombla <duncan@trombla.org>
2020-12-11 21:57:09 -0800

    fixed connection password

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "	nh0nnqz652m14hey",
+  user: "nh0nnqz652m14hey",
   password: "r7gura2p976fodko",
   database: "ycq434xmt7c04rga"
 });

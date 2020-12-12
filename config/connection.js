df00d3dc850e6921e8c6297b02cbdaa0c5894425
DTrombla <duncan@trombla.org>
2020-12-11 21:48:01 -0800

    fixed config file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "burger_db"
+  user: "	nh0nnqz652m14hey",
+  password: "r7gura2p976fodko",
+  database: "ycq434xmt7c04rga"
 });
 
 connection.connect(function(err) {
